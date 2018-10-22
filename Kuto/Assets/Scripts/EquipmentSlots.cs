c807495b87b401219d8be6b836bcba7259b2567a
Ricardo Cacheira <ricardo.cacheira@hotmail.com>
2018-10-22 16:54:04 +0100

    Inventory v0.1
    Can equip and unequip and switched if slot already used
    
    Skills and items are scriptable objects

---
+++ Kuto/Assets/Scripts/EquipmentSlots.cs
@@ -0,0 +1,12 @@
+﻿
+public class EquipmentSlots : ItemSlot {
+
+	public EquipmentType equipmentType;
+
+	protected override void OnValidate()
+	{
+		base.OnValidate();
+
+		gameObject.name = equipmentType.ToString() + " Slot";
+	}
+}
---
+++ Kuto/Assets/Scripts/ItemSlot.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ItemSlot : MonoBehaviour , IPointerClickHandler{ //to detect Clicks
+
+	//allows the variable to remain private and still show up in the editor, prenventing accidental changes to it in the code
+	[SerializeField] Image image;
+
+	public event Action<Item> OnRightClickEvent;
+
+	public Item _item;
+	public Item Item{
+		get{ return _item; }
+		set{
+			_item = value;
+			if(_item == null)
+			image.enabled = false;
+			else
+			{
+				image.sprite = _item.icon;
+				image.enabled = true;
+			}
+		}
+	}
+	
+
+
+	protected virtual void OnValidate()
+	{
+		//Only called on editor and triggers when script is loaded or change one of its values on the editor
+		//used this to fill the image variable for each slot
+		if(image==null)
+		image = GetComponent<Image>();
+
+	}
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+		if(eventData != null && eventData.button == PointerEventData.InputButton.Right)
+		{
+			if(Item != null && OnRightClickEvent != null)
+				OnRightClickEvent(Item);
+		}
+    }
+}

4e1029dd4a0a70828d77fb560f2d2072378128b3
Ricardo Cacheira <ricardo.cacheira@hotmail.com>
2018-10-23 23:25:23 +0100

    Inventory again

---
+++ Kuto/Assets/Scripts/EquippableItem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public enum EquipmentType {
+	Helmet,
+	Chest,
+	Gloves,
+	Boots
+}
+
+[CreateAssetMenu]
+public class EquippableItem : Item {
+
+	public int strenghtBonus;
+	public int vitalityBonus;
+
+	[Space] // Visible in unity editor
+	
+	// public Skill ability;
+
+	[Space] // Visible in unity editor
+	
+	public EquipmentType equipmentType;
+
+}
---
+++ Kuto/Assets/Scripts/ItemSlot.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ItemSlot : MonoBehaviour , IPointerClickHandler{ //to detect Clicks
+
+	//allows the variable to remain private and still show up in the editor, prenventing accidental changes to it in the code
+	[SerializeField] Image image;
+
+	public event Action<Item> OnRightClickEvent;
+
+	private Item _item;
+	public Item Item{
+		get{ return _item; }
+		set{
+			_item = value;
+			if(_item == null){
+				image.enabled = false;
+			}else{
+				image.sprite = _item.icon;
+				image.enabled = true;
+			}
+		}
+	}
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+		if(eventData != null && eventData.button == PointerEventData.InputButton.Right)
+		{
+			if(Item != null && OnRightClickEvent != null)
+			{
+				OnRightClickEvent(Item);
+			}
+		}
+    }
+
+    protected virtual void OnValidate()
+	{
+		//Only called on editor and triggers when script is loaded or change one of its values on the editor
+		//used this to fill the image variable for each slot
+		if(image==null)
+		image = GetComponent<Image>();
+
+	}
+}

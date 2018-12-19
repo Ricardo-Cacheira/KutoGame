c504fd62764301cf50afd42cb6122ec5828b0677
Ricardo Cacheira <ricardo.cacheira@hotmail.com>
2018-12-19 22:51:00 +0000

    tooltip color

--- Kuto/Assets/Scripts/ItemToolTip.cs
+++ Kuto/Assets/Scripts/ItemToolTip.cs
@@ -20,7 +20,7 @@ public class ItemToolTip : MonoBehaviour {
 		itemName.text = item.name.Substring(0,item.name.Length-7);
 		itemSlot.text = item.equipmentType.ToString();
 		// itemSkill.text = "SkillId: "+ item.skillID.ToString()+ " at Level "+item.level+"\nIt does stuff";
-		itemSkill.text = "Level "+ item.level + " " + c.abilities[item.skillID].name +"\n"+ c.abilities[item.skillID].description;
+		itemSkill.text = "<color=#23b2ff>Lvl. "+ item.level + "</color> " + c.abilities[item.skillID].name +"\n"+ c.abilities[item.skillID].description;
 
 		gameObject.SetActive(true);
 	}
